ad127d374b6d6dbda8efa2a9957bf91d7da901a3
Hedia ELkouni <hediakouni@gmail.com>
2026-09-13 11:29:54 +0200

    Add Dockerfiles for microservices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Profile from "./pages/Profile";
@@ -10,91 +10,22 @@ import Payment from "./pages/Payment";
 import Notifications from "./pages/Notifications";
 
 function Home() {
-  return (
-    <div>
-      <h1>Accueil</h1>
-      <p>Bienvenue sur notre plateforme e-commerce.</p>
-    </div>
-  );
-}
-
-// Protection des pages nécessitant une connexion
-function ProtectedRoute({ children }) {
-  const user = localStorage.getItem("user");
-
-  if (!user) {
-    return <Navigate to="/login" replace />;
-  }
-
-  return children;
+  return <h1>Accueil</h1>;
 }
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
-        {/* Pages publiques */}
         <Route path="/" element={<Home />} />
-
         <Route path="/login" element={<Login />} />
-
         <Route path="/register" element={<Register />} />
-
+        <Route path="/profile" element={<Profile />} />
         <Route path="/catalogue" element={<Catalogue />} />
-
-        {/* Pages protégées */}
-        <Route
-          path="/profile"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/cart"
-          element={
-            <ProtectedRoute>
-              <Cart />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/orders"
-          element={
-            <ProtectedRoute>
-              <Orders />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/payment"
-          element={
-            <ProtectedRoute>
-              <Payment />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/notifications"
-          element={
-            <ProtectedRoute>
-              <Notifications />
-            </ProtectedRoute>
-          }
-        />
-
-        {/* Page inexistante */}
-        <Route
-          path="*"
-          element={<Navigate to="/" replace />}
-        />
-
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/notifications" element={<Notifications />} />
       </Routes>
     </BrowserRouter>
   );
